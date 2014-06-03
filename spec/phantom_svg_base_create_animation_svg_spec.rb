2a93aa9fa7bc91fee07b211e12c71d5b7b3f7951
kou <kou@genshin.org>
2014-06-03 15:05:31 +0900

    Add new example to spec.

--- spec/phantom_svg_base_create_animation_svg_spec.rb
+++ spec/phantom_svg_base_create_animation_svg_spec.rb
@@ -1,11 +1,16 @@
 require 'phantom/svg'
 
+# Parameter.
+Source = "images/stuck_out_tongue/*.svg"
+DestinationDir = "tmp/stuck_out_tongue/"
+Destination = DestinationDir + "stuck_out_tongue.svg"
+
 # Set Current directory.
 Dir::chdir(File.dirname(__FILE__))
 
-# Parameter.
-Source = "images/stuck_out_tongue/*.svg"
-Destination = "stuck_out_tongue.svg"
+if File::exist?(DestinationDir) == false then
+  Dir::mkdir(DestinationDir)
+end
 
 # Test start.
 describe Phantom::SVG::Base, "when create animation svg (Source: \"" + Source + "\")" do
@@ -25,6 +30,17 @@ describe Phantom::SVG::Base, "when create animation svg (Source: \"" + Source + 
   it "animation svg file should have 12 frames" do
     @loader.load_file(Destination)
     @loader.frames.should have(12).frames
+  end
+
+  it "frame surface length should not equal 1" do
+    @loader.load_file(Destination)
+
+    for i in 0..(@loader.frames.length-1) do
+      frame = @loader.frames[i]
+      frame.surface.to_s.length.should_not eql(1)
+
+      @loader.save_frame(DestinationDir + i.to_s + ".svg", frame)
+    end
   end
 
   after do
