4b3cdc9f3a542f927f15ee348d7ddc5e536d9f5d
kou <kou@genshin.org>
2014-06-04 14:05:45 +0900

    Delete rubocop alert.

--- spec/phantom_svg_base_create_animation_svg_spec.rb
+++ spec/phantom_svg_base_create_animation_svg_spec.rb
@@ -1,45 +1,44 @@
 require 'phantom/svg'
 
 # Parameter.
-Source = "images/stuck_out_tongue/*.svg"
-DestinationDir = "tmp/stuck_out_tongue/"
-Destination = DestinationDir + "stuck_out_tongue.svg"
+SOURCE = 'images/stuck_out_tongue/*.svg'
+DESTINATION_DIR = 'tmp/stuck_out_tongue/'
+DESTINATION = DESTINATION_DIR + 'stuck_out_tongue.svg'
 
 # Set Current directory.
-Dir::chdir(File.dirname(__FILE__))
+Dir.chdir(File.dirname(__FILE__))
 
-if File::exist?(DestinationDir) == false then
-  Dir::mkdir(DestinationDir)
-end
+File.exist?(DESTINATION_DIR) || Dir.mkdir(DESTINATION_DIR)
 
 # Test start.
-describe Phantom::SVG::Base, "when create animation svg (Source: \"" + Source + "\")" do
+describe Phantom::SVG::Base, 'when create animation svg (SOURCE: \'' + SOURCE + '\')' do
   before do
-    @loader = Phantom::SVG::Base.new()
+    @loader = Phantom::SVG::Base.new
   end
 
-  it "save file size should not equal 0" do
-    files = Dir::glob(Source).sort_by { |k| k[/\d+/].to_i }
+  it 'save file size should not equal 0' do
+    files = Dir.glob(SOURCE).sort_by { |k| k[/\d+/].to_i }
     files.each do |file|
       @loader.create_frame(file)
     end
 
-    @loader.save(Destination).should_not eql(0)
+    @loader.save(DESTINATION).should_not eql(0)
   end
 
-  it "animation svg file should have 12 frames" do
-    @loader.load_file(Destination)
+  it 'animation svg file should have 12 frames' do
+    @loader.load_file(DESTINATION)
     @loader.frames.should have(12).frames
   end
 
-  it "frame surface length should not equal 1" do
-    @loader.load_file(Destination)
+  it 'frame surface length should not equal 1' do
+    @loader.load_file(DESTINATION)
 
-    for i in 0..(@loader.frames.length-1) do
+    range = 0..(@loader.frames.length - 1)
+    range.each do |i|
       frame = @loader.frames[i]
       frame.surface.to_s.length.should_not eql(1)
 
-      @loader.save_frame(DestinationDir + i.to_s + ".svg", frame)
+      @loader.save_frame(DestinationDir + i.to_s + '.svg', frame)
     end
   end
 
--- spec/phantom_svg_base_load_svg_spec.rb
+++ spec/phantom_svg_base_load_svg_spec.rb
@@ -1,42 +1,42 @@
 require 'phantom/svg'
 
 # Set Current directory.
-Dir::chdir(File.dirname(__FILE__))
+Dir.chdir(File.dirname(__FILE__))
 
 # Parameter.
-Source = "images/ninja.svg"
+SOURCE = 'images/ninja.svg'
 
 # Test start.
-describe Phantom::SVG::Base, "when load no animation svg (Source: \"" + Source + "\")" do
+describe Phantom::SVG::Base, 'when load no animation svg (SOURCE: \'' + SOURCE + '\')' do
   before do
-    @loader = Phantom::SVG::Base.new(Source)
+    @loader = Phantom::SVG::Base.new(SOURCE)
   end
 
-  it "frames should have 1 frames." do
+  it 'frames should have 1 frames.' do
     @loader.frames.should have(1).frames
   end
 
-  it "frame duration should equal 1." do
+  it 'frame duration should equal 1.' do
     @loader.frames[0].duration.should eql(1)
   end
 
-  it "frame surface should not be nil." do
+  it 'frame surface should not be nil.' do
     @loader.frames[0].surface.should_not be_nil
   end
 
-  it "frame surface should not be empty." do
+  it 'frame surface should not be empty.' do
     @loader.frames[0].surface.should_not be_empty
   end
 
-  it "frame width should equal \"64px\"." do
-    @loader.frames[0].width.should eql("64px")
+  it 'frame width should equal \'64px\'.' do
+    @loader.frames[0].width.should eql('64px')
   end
 
-  it "frame height should equal \"64px\"." do
-    @loader.frames[0].height.should eql("64px")
+  it 'frame height should equal \'64px\'.' do
+    @loader.frames[0].height.should eql('64px')
   end
 
-  it "frame namespaces should not be empty." do
+  it 'frame namespaces should not be empty.' do
     @loader.frames[0].namespaces.should_not be_empty
   end
 
