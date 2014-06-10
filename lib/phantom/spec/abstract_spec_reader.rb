51ca56e5ed9e3d1edfdc73fcfde4e437c676a064
kou <kou@genshin.org>
2014-06-10 16:18:48 +0900

    Moved source codes from JSONSpecReader to AbstractSpecReader.

--- lib/phantom/spec/abstract_spec_reader.rb
+++ lib/phantom/spec/abstract_spec_reader.rb
@@ -2,6 +2,110 @@
 module Phantom
   module Spec
     class AbstractSpecReader
+      # Construct AbstractSpecReader object.
+      def initialize
+        @name = ''
+        @loops = 0
+        @skip_first = false
+        @default_delay = 100.0 / 1000.0
+        @frame_infos = []
+        @delays = []
+      end
+
+      # Read and create frames from spec file.
+      # Return array of Phantom::SVG::Frame.
+      def read(path)
+        # Read parameter from spec file.
+        read_parameter(path)
+
+        # Change current directory to spec file's directory.
+        old_dir = Dir.pwd
+        Dir.chdir(File.dirname(path))
+
+        # Create frames from spec file parameter.
+        result = create_frames
+
+        # Change current directory to default.
+        Dir.chdir(old_dir)
+
+        # Return frames.
+        result
+      end
+
+      private
+
+      # Create frames from parameter.
+      # Return array of Phantom::SVG::Frame.
+      def create_frames
+        result = []
+        i = 0
+        @frame_infos.each do |frame_info|
+          create_file_list(frame_info[:name]).each do |file|
+            result << Phantom::SVG::Frame.new(file, create_options(i, frame_info[:delay]))
+            i += 1
+          end
+        end
+
+        result
+      end
+
+      # Create frame options.
+      def create_options(index, delay_override)
+        result = {}
+        result[:duration] =
+          if delay_override.nil?
+            if @delays.empty? then  @default_delay
+            else                    @delays[index % @delays.length]
+            end
+          else
+            delay_override
+          end
+        result
+      end
+
+      # Create file list.
+      def create_file_list(path)
+        result = Dir.glob(path).sort_by { |k| k[/\d+/].to_i }
+        if result.empty?
+          result <<
+            if    File.exist?(path)           then  path
+            elsif File.exist?(path + '.svg')  then  path + '.svg'
+            elsif File.exist?(path + '.png')  then  path + '.png'
+            else                                    path # Illegal path.
+            end
+        end
+        result
+      end
+
+      # Convert string to delay.
+      def str2delay(str)
+        tmp = str.to_s.split('/', 2)
+        tmp[0].to_f / (tmp.length > 1 ? tmp[1].to_f : 1000.0)
+      end
+
+      def set_name(name)
+        @name = name.to_s
+      end
+
+      def set_loops(loops)
+        @loops = loops.to_i
+      end
+
+      def set_skip_first(skip_first)
+        @skip_first = (skip_first.to_s == 'true' ? true : false)
+      end
+
+      def set_default_delay(default_delay)
+        @default_delay = str2delay(default_delay)
+      end
+
+      def add_frame_info(name, delay = nil)
+        @frame_infos << { name: name.to_s, delay: str2delay(delay) }
+      end
+
+      def add_delay(delay)
+        @delays << str2delay(delay)
+      end
     end # class AbstractSpecReader
   end # module Spec
 end # module Phantom
--- lib/phantom/spec/json_spec_reader.rb
+++ lib/phantom/spec/json_spec_reader.rb
@@ -7,36 +7,6 @@ require_relative 'abstract_spec_reader.rb'
 module Phantom
   module Spec
     class JSONSpecReader < AbstractSpecReader
-      # Construct JSONSpecReader object.
-      def initialize
-        @name = ''
-        @loops = 0
-        @skip_first = false
-        @default_delay = 100.0 / 1000.0
-        @frame_infos = []
-        @delays = []
-      end
-
-      # Read and create frames from spec file.
-      # Return array of Phantom::SVG::Frame.
-      def read(path)
-        # Read parameter from spec file.
-        read_parameter(path)
-
-        # Change current directory to spec file's directory.
-        old_dir = Dir.pwd
-        Dir.chdir(File.dirname(path))
-
-        # Create frames from spec file parameter.
-        result = create_frames
-
-        # Change current directory to default.
-        Dir.chdir(old_dir)
-
-        # Return frames.
-        result
-      end
-
       private
 
       # Read parameter from spec file.
@@ -44,12 +14,12 @@ module Phantom
         open(path) do |file|
           JSON.load(file).each do |key, val|
             case key
-            when 'name'           then  @name = val.to_s
-            when 'loops'          then  @loops = val.to_i
-            when 'skip_first'     then  @skip_first = val.to_s == 'true' ? true : false
-            when 'default_delay'  then  @default_delay = str2delay(val)
+            when 'name'           then  set_name(val)
+            when 'loops'          then  set_loops(val)
+            when 'skip_first'     then  set_skip_first(val)
+            when 'default_delay'  then  set_default_delay(val)
             when 'frames'         then  read_frame_infos(val)
-            when 'delays'         then  val.each { |delay| @delays << str2delay(delay) }
+            when 'delays'         then  val.each { |delay| add_delay(delay) }
             else                        # nop
             end
           end
@@ -61,61 +31,12 @@ module Phantom
         value.each do |frame_info|
           if frame_info.instance_of?(Hash)
             frame_info.each do |name, delay|
-              @frame_infos << { name: name, delay: str2delay(delay) }
+              add_frame_info(name, delay)
             end
           else
-            @frame_infos << { name: frame_info }
+            add_frame_info(frame_info)
           end
         end
-      end
-
-      # Create frames from parameter.
-      # Return array of Phantom::SVG::Frame.
-      def create_frames
-        result = []
-        i = 0
-        @frame_infos.each do |frame_info|
-          create_file_list(frame_info[:name]).each do |file|
-            result << Phantom::SVG::Frame.new(file, create_options(i, frame_info[:delay]))
-            i += 1
-          end
-        end
-
-        result
-      end
-
-      # Create frame options.
-      def create_options(index, delay_override)
-        result = {}
-        result[:duration] =
-          if delay_override.nil?
-            if @delays.empty? then  @default_delay
-            else                    @delays[index % @delays.length]
-            end
-          else
-            delay_override
-          end
-        result
-      end
-
-      # Create file list.
-      def create_file_list(path)
-        result = Dir.glob(path).sort_by { |k| k[/\d+/].to_i }
-        if result.empty?
-          result <<
-            if    File.exist?(path)           then  path
-            elsif File.exist?(path + '.svg')  then  path + '.svg'
-            elsif File.exist?(path + '.png')  then  path + '.png'
-            else                                    path # Illegal path.
-            end
-        end
-        result
-      end
-
-      # Convert string to delay.
-      def str2delay(str)
-        tmp = str.to_s.split('/', 2)
-        tmp[0].to_f / (tmp.length > 1 ? tmp[1].to_f : 1000.0)
       end
     end # class JSONSpecReader < AbstractSpecReader
   end # module Spec
